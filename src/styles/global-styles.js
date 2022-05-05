03e09bc003f863caaacfe1b0e36ae7f5065e0769
IsisG13 <isissouzaguimaraes@gmail.com>
2022-05-05 16:09:01 -0300

    criação do componente Heading (pt 2)

--- src/styles/global-styles.js
+++ src/styles/global-styles.js
@@ -14,6 +14,6 @@ export const GlobalStyles = createGlobalStyle`
     font-family: ${({ theme }) => theme.font.family.default};
   }
   h1, h2, h3, h4, h5, h6 {
-    font-family: ${({ theme }) => theme.font.family.secondary};
+    font-family: 'Montserrat', sans-serif;
   }
 `;
