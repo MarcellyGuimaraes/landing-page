ab3b21d392915066b2916e248d80fb5b6135a7bf
IsisG13 <isissouzaguimaraes@gmail.com>
2022-05-05 15:20:09 -0300

    criação do componente Heading (pt 1)

--- src/components/Heading/styles.js
+++ src/components/Heading/styles.js
@@ -1,6 +1,4 @@
-import styled from 'styled-components';
-import { css } from 'styled-components';
-
+import styled, { css } from 'styled-components';
 const titleSize = {
   small: (theme) => css`
     font-size: ${theme.font.sizes.medium};
@@ -13,17 +11,23 @@ const titleSize = {
   `,
   huge: (theme) => css`
     font-size: ${theme.font.sizes.xhuge};
+    ${mediaFont(theme)};
   `,
 };
+
+const mediaFont = (theme) => css`
+  @media ${theme.media.lteMedium} {
+    font-size: ${theme.font.sizes.xlarge};
+  }
+`;
 
 const titleCase = (uppercase) => css`
   text-transform: ${uppercase ? 'uppercase' : 'none'};
 `;
-
 export const Title = styled.h1`
   ${({ theme, colorDark, size, uppercase }) => css`
     color: ${colorDark ? theme.colors.primaryColor : theme.colors.white};
-    ${titleSize[size](theme)}
-    ${titleCase(uppercase)}
+    ${titleSize[size](theme)};
+    ${titleCase(uppercase)};
   `}
 `;
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -2,17 +2,15 @@ export const theme = {
   colors: {
     primaryColor: '#0A1128',
     secondaryColor: '#dc143c',
-    white: '#FFF',
+    white: '#FFFFFF',
   },
-
   font: {
     family: {
-      primary: "'Open Sans', sans-serif",
+      default: "'Open Sans', sans-serif",
       secondary: "'Montserrat', sans-serif",
     },
-
     sizes: {
-      xsmall: '0.8rem',
+      xsmall: '8rem',
       small: '1.6rem',
       medium: '2.4rem',
       large: '3.2rem',
@@ -21,5 +19,8 @@ export const theme = {
       huge: '5.6rem',
       xhuge: '6.4rem',
     },
+  },
+  media: {
+    lteMedium: '(max-width: 768px)',
   },
 };
