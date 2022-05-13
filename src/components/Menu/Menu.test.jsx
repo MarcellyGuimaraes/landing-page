af5b70fc9c6481de39929a758a013ebbf9f49e37
IsisG13 <isissouzaguimaraes@gmail.com>
2022-05-13 18:58:28 -0300

    Alteração NavLinks

--- src/components/Menu/Menu.test.jsx
+++ src/components/Menu/Menu.test.jsx
@@ -2,9 +2,15 @@ import { screen } from '@testing-library/react';
 import { renderTheme } from '../../styles/render-theme';
 import { Menu } from '.';
 
+import linksMock from '../NavLinks/mock';
+const logoData = {
+  text: 'Logo',
+  link: '#target',
+};
+
 describe('<Menu />', () => {
   it('should render', () => {
-    renderTheme(<Menu>Children</Menu>);
+    renderTheme(<Menu links={linksMock} logoData={logoData} />);
     expect(screen.getByRole('heading')).toBeInTheDocument();
   });
 });
